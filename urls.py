6dcbef180de36c824c047dc93fc4ea4bc4398ee8
MarcoC2161 <sh216123@gmail.com>
2024-10-16 21:43:14 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index), #views파일에서 index함수를 가져오라는 의미
-    path('menu/<str:food>/', views.food_detail),
-]
